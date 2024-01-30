9fb6ac67603d0b01fa00f126506ab5eab26773c5
Hernán Ezequiel Novara <her.novara@hotmail.com>
2024-01-29 22:36:30 -0300

    js

--- js/calculator.js
+++ js/calculator.js
@@ -1,16 +1,25 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
+
 buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
         if (btn.id === "=") {
-            display.value = eval(display.value);
+            try {
+                display.value = eval(display.value);
+                if (!isFinite(display.value)) {
+                    throw new Error("Resultado no válido");
+                }
+            } catch (error) {
+                display.value = "Error";
+            }
         } else if (btn.id === "ac") {
-            display.value = "";
+           display.value = "";
         } else if (btn.id === "de") {
             display.value = display.value.slice(0, -1);
-        } else {
-            display.value += btn.id
+         } else {
+            display.value += btn.id;
         }
     })
 })
+    
